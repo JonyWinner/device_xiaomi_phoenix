a67211082154c233f6b2ba88cdb823b1e7d61e26
JonyWinner <15685367981@163.com>
2022-06-29 09:30:23 +0000

    phoenix:Fix a typo error
    Signed-off-by: JonyWinner <15685367981@163.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,5 @@ git clone https://github.com/chiru2000/vendor_xiaomi_phoenix --depth=1 vendor/xi
 echo "Cloning Prelude Clang......"
 git clone https://gitlab.com/jjpprrrr/prelude-clang --depth=1 prebuilt/clang/host/linux-x86/clang-prelude
 echo "Cloning some extras......"
-git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras device/xiaomi/phoenix
+git clone https://github.com/ArrowOS-Devices/android_device_xiaomi_extras device/xiaomi/extras
 
